ca1c40d7fa8d7875aec11d7672f811f2951a2930
Peter Levinsky <pele@zealand.dk>
2025-02-14 09:45:41 +0100

    Add project files.

---
+++ ClassDemoConfigogLog/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoConfigogLog;
+
+Console.WriteLine("Hello, World!");
+
+ReadConfig cnf = new ReadConfig();
+cnf.Start();
+
+//LoggingClass log = new LoggingClass();
+//log.Start();
+
---
+++ ClassDemoConfigogLog/ReadConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ClassDemoConfigogLog
+{
+    public  class ReadConfig
+    {
+
+        public void Start()
+        {
+            string name = "";
+            int port = 0;
+
+            XmlDocument configXML = new XmlDocument();
+            configXML.Load("../../../Config.xml");
+
+            XmlNode? NameXml = configXML.DocumentElement.SelectSingleNode("Name");
+            if (NameXml != null)
+            {
+                name = NameXml.InnerText;
+            }
+
+            XmlNode? PortXml = configXML.DocumentElement.SelectSingleNode("Port");
+            if (PortXml != null)
+            {
+                string portTxt = PortXml.InnerText.Trim();
+                port = Convert.ToInt32(portTxt);
+            }
+
+
+            Console.WriteLine("Name is " + name + " the port number is " + port);
+        }
+    }
+}
